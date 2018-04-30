c805e1d0706cc2ed2554123fe888b2f6fc0716cc
Goncalo Baptista <gms_baptista@sapo.pt>
2018-04-30 21:54:49 +0100

    Map stuf and collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     private int worldIndex = -1; //-1 is physical, 1 is spirit
     public float worldCenter;
     private Animator animator;
+    private Rigidbody2D rigidBody;
     private bool moving;
     private Vector2 lastMove;
 
@@ -16,18 +17,35 @@ public class Player : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moving = false;
-        if (Input.GetAxisRaw("Horizontal") != 0f || Input.GetAxisRaw("Vertical") != 0f)
+        if (Input.GetAxisRaw("Horizontal") != 0f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f));
+            rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rigidBody.velocity.y);
             moving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        }        
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+        if (Input.GetAxisRaw("Vertical") != 0f)
+        {
+            //transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            moving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0f)
+        {
+            rigidBody.velocity = new Vector2(0f, rigidBody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") == 0f)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
+        }
         animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
         animator.SetFloat("LastMoveX", lastMove.x);
--- Assets/Tilesets/floor.tsx
+++ Assets/Tilesets/floor.tsx
@@ -1,4 +1,92 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="floor" tilewidth="16" tileheight="16" tilecount="819" columns="21">
  <image source="../Sprites/Objects/Floor.png" width="336" height="624"/>
+ <tile id="385">
+  <objectgroup draworder="index">
+   <object id="8" x="0.25" y="0.125" width="3.25" height="15.875"/>
+   <object id="9" x="0.375" y="0.125" width="15.625" height="3.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="386">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="15.625" height="4.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="387">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.75" height="4.5"/>
+   <object id="2" x="12.25" y="0.125" width="3.625" height="15.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="406">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0" width="3.625" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="408">
+  <objectgroup draworder="index">
+   <object id="1" x="11.75" y="0" width="4.125" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="427">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.125" width="3.875" height="16.125"/>
+   <object id="2" x="0.25" y="11.875" width="15.75" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="428">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="12" width="16.125" height="4.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="429">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="12.125" width="16.25" height="3.625"/>
+   <object id="2" x="12" y="0" width="3.625" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="511">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0.125" width="4.125" height="15.875"/>
+   <object id="3" x="0.375" y="0.125" width="15.625" height="4.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="512">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0" width="16" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="513">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="15.75" height="3.875"/>
+   <object id="2" x="12.125" y="-0.375" width="3.875" height="16.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="532">
+  <objectgroup draworder="index">
+   <object id="3" x="-0.125" y="0" width="4.25" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="534">
+  <objectgroup draworder="index">
+   <object id="1" x="12.125" y="-0.125" width="3.875" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="553">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="11.875" width="15.875" height="4.25"/>
+   <object id="2" x="0.125" y="0.125" width="4.125" height="16.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="554">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="11.875" width="16" height="4.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="555">
+  <objectgroup draworder="index">
+   <object id="1" x="12.125" y="0" width="3.625" height="15.75"/>
+   <object id="2" x="-0.125" y="12.25" width="15.875" height="3.75"/>
+  </objectgroup>
+ </tile>
 </tileset>
