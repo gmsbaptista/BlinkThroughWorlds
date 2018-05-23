dff0c4f57744443f54f048429ebc49a1a131077d
JoanaArezes <joanarezes@outlook.com>
2018-05-23 12:01:34 +0100

    Add complete level 1 map

---
+++ Assets/Maps/bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bg" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Tilesets/tile2.png" width="32" height="32"/>
+</tileset>
---
+++ Assets/Maps/spirit_world.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spirit_world" tilewidth="32" tileheight="32" spacing="1" tilecount="9" columns="3">
+ <image source="../Tilesets/sw_tile.png" width="98" height="98"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.5" y="-2" width="36.25" height="3.25"/>
+   <object id="2" x="-3.5" y="-3.75" width="5.5" height="37.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.5" y="-1.75" width="33.25" height="3.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.5" y="-2" width="34" height="3.5"/>
+   <object id="2" x="30.75" y="-4.5" width="2.25" height="40"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.75" y="-4" width="4.75" height="38.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="30" y="-4" width="2.25" height="37.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.5" y="30.5" width="36" height="3.75"/>
+   <object id="2" x="-2.25" y="-1.5" width="4" height="35.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.75" y="30.5" width="34.5" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="3" x="30.25" y="-2.75" width="2.5" height="37.25"/>
+   <object id="4" x="-3.5" y="31" width="38.25" height="6.25"/>
+  </objectgroup>
+ </tile>
+</tileset>
