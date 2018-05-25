f19a093489b4b51fb77a3ddde4af2b38f72af446
JoanaArezes <joanarezes@outlook.com>
2018-05-25 14:11:38 +0100

    Fucking map collisions

--- Assets/Maps/real_world.tsx
+++ Assets/Maps/real_world.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="real_world_tile" tilewidth="32" tileheight="32" spacing="1" tilecount="9" columns="3">
- <image source="../Tilesets/rw_tile.png" width="98" height="98"/>
+<tileset name="real_world_tile" tilewidth="32" tileheight="32" spacing="1" tilecount="12" columns="4">
+ <image source="../Tilesets/rw_tile.png" width="131" height="98"/>
  <tile id="0">
   <objectgroup draworder="index">
    <object id="5" x="-2" y="-1.25" width="34.25" height="2.75"/>
@@ -18,28 +18,33 @@
    <object id="2" x="30.5" y="-4.75" width="3.5" height="38.25"/>
   </objectgroup>
  </tile>
- <tile id="3">
+ <tile id="4">
   <objectgroup draworder="index">
    <object id="1" x="-1.5" y="-1" width="3.25" height="40.5"/>
   </objectgroup>
  </tile>
- <tile id="5">
+ <tile id="6">
   <objectgroup draworder="index">
    <object id="1" x="30.5" y="-3.75" width="2.5" height="38"/>
   </objectgroup>
  </tile>
- <tile id="6">
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="27.25" y="-4.5" width="6.5" height="37"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
   <objectgroup draworder="index">
    <object id="1" x="-5.75" y="30.25" width="39.5" height="2.75"/>
    <object id="2" x="-1.75" y="-0.25" width="3.25" height="34.5"/>
   </objectgroup>
  </tile>
- <tile id="7">
+ <tile id="9">
   <objectgroup draworder="index">
    <object id="1" x="-3.5" y="30.5" width="38.25" height="4.75"/>
   </objectgroup>
  </tile>
- <tile id="8">
+ <tile id="10">
   <objectgroup draworder="index">
    <object id="1" x="30.75" y="-2" width="2.25" height="34.25"/>
    <object id="2" x="-3" y="30.75" width="36.5" height="4.75"/>
--- Assets/Maps/spirit_world.tsx
+++ Assets/Maps/spirit_world.tsx
@@ -1,4 +1,48 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="spirit_world" tilewidth="32" tileheight="32" spacing="1" tilecount="12" columns="4">
  <image source="../Tilesets/rw_tile2.png" width="131" height="98"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.5" y="-5.25" width="36.5" height="8.5"/>
+   <object id="3" x="-2.5" y="-9.25" width="5.25" height="42.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="-4.5" y="-5.25" width="41" height="8.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.5" y="-3.25" width="36.5" height="6.75"/>
+   <object id="2" x="28.75" y="-7" width="5.75" height="43"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.25" y="-6.25" width="5.5" height="49.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="28.5" y="-3" width="6.25" height="39.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="-3" y="27" width="34.75" height="8.75"/>
+   <object id="2" x="-2.5" y="-4" width="6" height="50.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="-5.25" y="29" width="40.25" height="7.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="29.5" y="-6.25" width="4.75" height="40.5"/>
+   <object id="2" x="-4.25" y="29" width="41.5" height="8"/>
+  </objectgroup>
+ </tile>
 </tileset>
